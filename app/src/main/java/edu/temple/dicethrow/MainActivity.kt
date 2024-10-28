9a6926fb8662f387660c87538f9a6592857bb8bf
ekourtakis <emmanuel.kourtakis@temple.edu>
2024-10-28 15:37:48 -0400

    completed TODO 1

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,10 +1,9 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 
 /*
@@ -19,6 +18,8 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+    var hasTwoContainers = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +28,22 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show only Button Fragment if portrait
             - show both fragments if Landscape
           */
+        hasTwoContainers = findViewById<View>(R.id.container2) != null
+        val dieViewModel = ViewModelProvider(this)[DieViewModel::class.java]
+        val buttonFragment = ButtonFragment()
+
+        if (savedInstanceState == null)
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container1, buttonFragment)
+                .commit()
+
+        if (hasTwoContainers)
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container2, DieFragment())
+                .commit()
+
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
